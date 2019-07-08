71a6383cbe29e12c36d8954149695da3c0b74df8
Rafael <rreynoud@gmail.com>
2019-07-08 19:58:51 -0300

    First commit

---
+++ src/main/java/com/minicurso/unicentro/UnicentroApplication.java
@@ -0,0 +1,13 @@
+package com.minicurso.unicentro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicentroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicentroApplication.class, args);
+	}
+
+}
