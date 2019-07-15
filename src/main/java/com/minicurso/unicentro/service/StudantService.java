1416fafcda83130ccce4a61fd3170a0254dd405d
Rafael <rreynoud@gmail.com>
2019-07-15 20:03:53 -0300

    Criado repository e service para studant

---
+++ src/main/java/com/minicurso/unicentro/service/StudantService.java
@@ -0,0 +1,9 @@
+package com.minicurso.unicentro.service;
+
+import com.minicurso.unicentro.model.Student;
+
+import java.util.List;
+
+public interface StudantService {
+    public List<Student> findAll();
+}
